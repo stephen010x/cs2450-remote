f43ff1f6857d21086aba0ebcad6fd4f6b7967f43
stephen010x <stephen010x@gmail.com>
2025-01-14 15:38:43 -0700

    added more fun features

--- age.py
+++ age.py
@@ -4,9 +4,15 @@ print("Hello there, err...")
 print("How uhh... how old are you?")
 
 age = -1
-guess = 0
+guesses = list(range(1,31))
+random.shuffle(guesses)
+
 while True:
-    guess = random.randint(1,30)
+    if len(guesses) == 0:
+        break;
+    
+    guess = guesses.pop(-1)
+    
     test = input(f"are you {guess}? (y/n) ")
 
     if test == 'y':
@@ -15,11 +21,14 @@ while True:
     elif test == 'n':
         continue;
     else:
+        guesses.append(guess)
         print("please just answer me y/n")
 
 if age >= 18:
     print("thank god")
-if age >= 8:
+elif age >= 8:
     print("oh. I shouldn't be talking to you.")
-else:
+elif age > 0:
     print("I don't believe you")
+else:
+    print("alright, I give up")
